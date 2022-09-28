9dbef72b06375711bc63e367ceba7eb80b852c01
max-derlow <max.derlow@gmail.com>
2022-09-28 21:53:56 +0200

    improved toy buttons' usability

--- script.js
+++ script.js
@@ -71,10 +71,12 @@ const btnDraw = document.querySelector("#pencil");
 const btnEraser = document.querySelector("#eraser");
 const btnSave = document.querySelector("#save");
 const btnClear = document.querySelector("#clear");
-const btnRainbow = document.querySelector("#rainbow");
-const btnBucket = document.querySelector("#bucket");
-const btnBomb = document.querySelector("#bomb");
-
+const btnRainbow = document.querySelector("#btnRainbow");
+const btnBucket = document.querySelector("#btnBucket");
+const btnBomb = document.querySelector("#btnBomb");
+const imgRainbow = document.querySelector("#imgRainbow");
+const imgBucket = document.querySelector("#imgBucket");
+const imgBomb = document.querySelector("#imgBomb");
 
 const lblSlider = document.querySelector("#lblSlider");
 const gridSlider = document.querySelector("#gridSlider");
@@ -82,7 +84,6 @@ const colorPicker = document.querySelector("#colorPicker");
 
 let currentColor = colorPicker.value;
 
-console.log(colorPicker);
 
 //functions
 function saveAs(uri, filename) {
@@ -111,11 +112,17 @@ function saveAs(uri, filename) {
 }
 
 function clearGrid() {
+    document.querySelectorAll(".gridItem").forEach((e) => {
+        e.remove();
+    });
+/*
     gridItems.forEach((e) => {
         grid.removeChild(e);
     })
+    */
     gridItems = [];
     generateGridItems();
+
 }
 
 //set listeners
@@ -161,23 +168,27 @@ btnClear.addEventListener('click', clearGrid);
 btnRainbow.addEventListener('click', (e) => {
     deactivateButtons();
     btnRainbow.classList.toggle('active');
+    imgRainbow.classList.toggle('invert');
     Pencil.setRainbowMode = true;
 });
 
 btnBucket.addEventListener('click', (e) => {
     deactivateButtons();
     btnBucket.classList.toggle('active');
+    imgBucket.classList.toggle('invert');
     Pencil.setBucketMode = true;
 });
 
 btnBomb.addEventListener('click', (e) => {
     deactivateButtons();
     btnBomb.classList.toggle('active');
+    imgBomb.classList.toggle('invert');
     Pencil.setBombMode = true;
 });
 
 function deactivateButtons() {
     document.querySelectorAll(".btnSettings, .btnToy").forEach((e) => e.classList.remove('active'));
+    document.querySelectorAll(".btnIcon").forEach((e) => e.classList.remove('invert'));
     Pencil.resetAttributes();
 }
 
@@ -198,27 +209,29 @@ window.addEventListener("mouseup", () => {
 
 
 function generateGridItems() {
-    for(let x = 0; x < gridSize; x++){
+    for(let y = 0; y < gridSize; y++){
         let itemSize = (1/gridSize) * 100; 
-        for(let y = 0; y < gridSize; y++) {
-            let item_id = `${x}${y}`;
+        for(let x = 0; x < gridSize; x++) {
+            let item_id = `${(y*gridSize)+x}`;
             const item = document.createElement('div');
             item.classList.add('gridItem');
+            item.setAttribute('id', item_id);
+            //console.log(item);
             item.style.width = `${itemSize}%`;
             item.style.height = `${itemSize}%`;
             item.addEventListener("mousedown", handleInput);
             item.addEventListener("mouseover", handleInput);
             grid.appendChild(item);
-            gridItems.push(item);
+            gridItems.push({x:x, y:y, id:item_id});
         }
     }
 }
 
-function draw(gridItem, rainbowMode) {
+function draw(itemIndex, rainbowMode) {
+    gridItem = document.querySelector(`[id='${itemIndex}']`)
     if(rainbowMode) {
         let randomColor = Math.floor(Math.random()*16777215).toString(16);
         gridItem.style.backgroundColor = `#${randomColor}`;
-        console.log(randomColor);
     } else {
         gridItem.style.backgroundColor = currentColor;
     }
@@ -228,9 +241,114 @@ function erase(gridItem) {
     gridItem.style.backgroundColor = defaultColor;
 }
 
-function bucket(gridItem) {
+function checkIfSameColor(knownItemIndex, unknownItemIndex){
+    knownItemIndex = parseInt(knownItemIndex);
+    unknownItemIndex = parseInt(unknownItemIndex);
 
+    console.log(knownItemIndex);
+    console.log(unknownItemIndex);
+    const knownColor = document.querySelector(`[id='${knownItemIndex}']`).style.backgroundColor;
+    console.log("known color:" + knownColor);
+    const unknownColor = document.querySelector(`[id='${unknownItemIndex}']`).style.backgroundColor;
+    console.log("unknown color:" + unknownColor);
+
+    //draw(knownItemIndex, false);
+
+    if(knownColor === unknownColor) {
+        return true;
+    } else {
+        return false;
+    }
 }
+
+function findAdjacentsToPaint(itemIndex){
+    //console.log("checked indexes:");
+    //console.log(checkedIndexes);
+    itemIndex = parseInt(itemIndex);
+    gridSize = parseInt(gridSize);
+    console.log(itemIndex)
+    console.log(typeof(itemIndex))
+    let adjacentsInfo = {};
+    let leftIndex;
+    let rightIndex; 
+    let topIndex;
+    let botIndex;
+    let itemX = gridItems[itemIndex].x;
+    let itemY = gridItems[itemIndex].y;
+
+    if(itemX - 1 >= 0){
+        leftIndex = itemIndex - 1;
+        Object.assign(adjacentsInfo, {[leftIndex]: checkIfSameColor(itemIndex, leftIndex)});
+    }
+
+    if(itemX + 1 < gridSize){
+        rightIndex = itemIndex + 1;
+        Object.assign(adjacentsInfo, {[rightIndex]: checkIfSameColor(itemIndex, rightIndex)});
+    }
+
+    if(itemY - 1 >= 0){
+        topIndex = itemIndex - gridSize;
+        Object.assign(adjacentsInfo, {[topIndex]: checkIfSameColor(itemIndex, topIndex)});
+    }
+
+    if(itemY + 1 < gridSize){
+        botIndex = itemIndex + gridSize;
+        Object.assign(adjacentsInfo, {[botIndex]: checkIfSameColor(itemIndex, botIndex)});
+    }
+    //console.log(adjacentsInfo)
+    return adjacentsInfo;
+}
+
+/*
+    indexesToCheck.push(itemIndex);
+    indexesToCheck.push(itemIndex+1);
+    console.log("pushing to array");
+    console.log(indexesToCheck);*/
+
+function bucket(itemIndex) {
+    let checkedIndexes = [];
+    let indexesToCheck = [];
+    let indexesToPaint = [];
+    let itemX = gridItems[itemIndex].x;
+    let itemY = gridItems[itemIndex].y;
+    let compareIndex;
+
+    indexesToCheck.push(itemIndex);
+    indexesToPaint.push(itemIndex);
+
+    let i = 0;
+    while(indexesToCheck.length > 0) {
+        let index = indexesToCheck[0];
+
+        if(checkedIndexes.includes(index)){
+            indexesToCheck.shift();
+            console.log("removing value" + i)
+            continue;
+        } else {
+            checkedIndexes.push(index)
+            indexesToCheck.shift();
+        } 
+
+        console.log("Sending: " + index)
+        let adjacentsInfo = findAdjacentsToPaint(index);
+        console.log(adjacentsInfo);
+
+        Object.keys(adjacentsInfo).forEach((adjIndex) => {
+            console.log("Hero")
+            console.log(adjacentsInfo)
+            console.log(!checkedIndexes.includes(adjIndex))
+
+            if(adjacentsInfo[adjIndex] && !checkedIndexes.includes(adjIndex)) {
+                indexesToPaint.push(adjIndex);
+                indexesToCheck.push(adjIndex);
+            } else {
+                checkedIndexes.push(adjIndex);
+            }
+        })
+    }
+    indexesToPaint.forEach((index) => draw(parseInt(index), false))
+}
+
 
 function bomb(gridItem) {
 
@@ -240,33 +358,28 @@ function handleInput(e) {
     if(e.type === 'mouseover' && !mouseDown) {return;}
     switch(true) {
         case Pencil.getDrawMode:
-            draw(this, false);
-            console.log("in draw mode");
+            draw(this.id, false);
+            //console.log("in draw mode");
             break;
         case Pencil.getEraseMode:
-            erase(this)
-            console.log("in erase mode");
+            erase(this.id)
+            //console.log("in erase mode");
             break;
         case Pencil.getRainbowMode:
-            draw(this, true);
-            console.log("in rainbow mode");
+            draw(this.id, true);
+            //console.log("in rainbow mode");
             break;
         case Pencil.getBucketMode:
-            console.log("in bucket mode");
+            bucket(this.id)
+            //console.log("in bucket mode");
             break;
         case Pencil.getBombMode:
-            console.log("in bomb mode");
+            //console.log("in bomb mode");
             break;
         default:
-            console.log("unga bunga you gucked it up.");
+            //console.log("unga bunga you gucked it up.");
     }    
 }
-
-
-
-//function to draw (event listener)
-
-//function to erase
 
 generateGridItems();
 
